34704fb5c4e21d2965d3e8b231dfd29a3ff9045d
sahilkhan876 <120260634+sahilkhan876@users.noreply.github.com>
2023-01-25 17:21:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5883676435:AAGJ3UbHTGySx185h-_pZMOcxvg5ENQn9yE")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCSbgHk__WTJtTWYJozLgrVsEygpBr62qR_mxaGUcFF5U41ZERdZlQAUA0Svpa4AsrR0VqYTuvFijP1IeLPqOcR58QxP6bYDF6sIYAlhrzUGaa0ZEZC2QW_tcA3G-pFCkXBeGmdPGSmI9AlYxNIN3dlnCB9P27lrKhWXmetxCxlEN_s8KFMDJi-W1jOTDWs5F7Lhwas69TDDEtyivLLNJT2Pex2beY-jXlDS397DAJg4MHkr2-1Z2Dn-bJi-zoRjqASCcoscwrMaerOPXLXmTxn0YfHQeGtXPw25e7HydUSx_Y8YvAVpkb8YoBmib_8fXUbYdoqDbjbofDxNOpWJpZ8AAAAAUoOq68A")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001685615060"))
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mdsik_moviebot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "5537442735"))
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Mdsikmoviebot:Mdsikmoviebot@cluster0.xwbcpg3.mongodb.net/?retryWrites=true&w=majority")
