12b9dc3782628cf5eb89ab8b4875f368f51a2d36
Uwiejsle <115135419+Mokshb6458@users.noreply.github.com>
2023-01-25 16:43:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,21 +2,21 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "26506890"))
+    API_HASH = os.environ.get("API_HASH", "5839207dcf3b270c58d88d5de6830bac")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5883676435:AAGJ3UbHTGySx185h-_pZMOcxvg5ENQn9yE")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCSbgHk__WTJtTWYJozLgrVsEygpBr62qR_mxaGUcFF5U41ZERdZlQAUA0Svpa4AsrR0VqYTuvFijP1IeLPqOcR58QxP6bYDF6sIYAlhrzUGaa0ZEZC2QW_tcA3G-pFCkXBeGmdPGSmI9AlYxNIN3dlnCB9P27lrKhWXmetxCxlEN_s8KFMDJi-W1jOTDWs5F7Lhwas69TDDEtyivLLNJT2Pex2beY-jXlDS397DAJg4MHkr2-1Z2Dn-bJi-zoRjqASCcoscwrMaerOPXLXmTxn0YfHQeGtXPw25e7HydUSx_Y8YvAVpkb8YoBmib_8fXUbYdoqDbjbofDxNOpWJpZ8AAAAAUoOq68A")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mdsik_moviebot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5537442735"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Mdsikmoviebot:Mdsikmoviebot@cluster0.xwbcpg3.mongodb.net/?retryWrites=true&w=majority")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001775772199")
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     
     
     
-🤖 My Name: <a href='https://youtube.com/@GreyMattersBot'>Link Search Bot</a>
+🤖 My Name: <a href='https://youtube.com/@GreyMattersYT'>Link Search Bot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -24,10 +24,10 @@ class Config(object):
 
 📡 Server: <a href='koyeb.com'>Koyeb</a>
 
-👨‍💻 Created By: <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a></b>
+👨‍💻 Created By: <a href='https://t.me/GreyMatter_owner'>GreyMatters</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/GreyMatter_Owner'>GreyMatters</a>
 If You Want Your Own Bot Like This Then You Can Contact Our Creator.</b>
 """
 
